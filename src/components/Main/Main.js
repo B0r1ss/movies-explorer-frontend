ec3178bbd2f1f448075d462de14cbffa77aa9cb6
B0r1ss <borisenko.va90@gmail.com>
2021-09-22 18:26:26 +0300

    init

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,19 @@
 import Header from "../Header/Header";
 import Promo from "../Promo/Promo";
+import NavTab from "../NavTab/NavTab";
+import AboutProject from "../AboutProject/AboutProject";
 
 
 function Main(props) {
     return (
     <>
        <Promo />
-
+       <NavTab />
+       <AboutProject />
+ {/*       <Techs />
+       <AboutMe />
+       <Portfolio />
+        <Footer /> */}
     </>
 
     )
---
+++ src/components/NavTab/NavTab.js
@@ -0,0 +1,19 @@
+export default function NavTab() {
+  return (
+    <section className="navTab">
+      <nav className="navTab__nav">
+        <ul className="navTab__links">
+          <li className="navTab__list">
+            <a className="navTab__link" href="#about">О проекте</a>
+          </li>
+          <li className="navTab__list">
+            <a className="navTab__link" href="#"> Технологии</a>
+          </li>
+          <li lassName="navTab__list">
+            <a className="navTab__link" href="#">Студент</a>
+          </li>
+      </ul>
+      </nav>
+    </section>
+  )
+} 
